963ec9e307a77dce0fb3e8faac0c969f95124801
mantaionut <ionut@janeasystems.com>
2024-12-18 13:05:46 +0200

    Revert "[Logs]Fixed name of logging method in managed logging (#35952)"
    This reverts commit 08fe8089b84468b66628af221bffddd1750e02f0.

--- src/common/ManagedCommon/Logger.cs
+++ src/common/ManagedCommon/Logger.cs
@@ -7,7 +7,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 using PowerToys.Interop;
 
@@ -53,18 +52,16 @@ namespace ManagedCommon
             Trace.AutoFlush = true;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void LogError(string message)
         {
             Log(message, Error);
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void LogError(string message, Exception ex)
         {
             if (ex == null)
             {
-                Log(message, Error);
+                LogError(message);
             }
             else
             {
@@ -87,31 +84,26 @@ namespace ManagedCommon
             }
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void LogWarning(string message)
         {
             Log(message, Warning);
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void LogInfo(string message)
         {
             Log(message, Info);
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void LogDebug(string message)
         {
             Log(message, Debug);
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void LogTrace()
         {
             Log(string.Empty, TraceFlag);
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         private static void Log(string message, string type)
         {
             Trace.WriteLine("[" + DateTime.Now.TimeOfDay + "] [" + type + "] " + GetCallerInfo());
@@ -124,49 +116,13 @@ namespace ManagedCommon
             Trace.Unindent();
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         private static string GetCallerInfo()
         {
             StackTrace stackTrace = new();
 
-            var callerMethod = GetCallerMethod(stackTrace);
-
-            return $"{callerMethod?.DeclaringType?.Name}::{callerMethod.Name}";
-        }
-
-        private static MethodBase GetCallerMethod(StackTrace stackTrace)
-        {
-            const int topFrame = 3;
-
-            var topMethod = stackTrace.GetFrame(topFrame)?.GetMethod();
-
-            try
-            {
-                if (topMethod?.Name == nameof(IAsyncStateMachine.MoveNext) && typeof(IAsyncStateMachine).IsAssignableFrom(topMethod?.DeclaringType))
-                {
-                    // Async method; return actual method as determined by heuristic:
-                    // "Nearest method on stack to async state-machine's MoveNext() in same namespace but in a different type".
-                    // There are tighter ways of determining the actual method, but this is good enough and probably faster.
-                    for (int deepFrame = topFrame + 1; deepFrame < stackTrace.FrameCount; deepFrame++)
-                    {
-                        var deepMethod = stackTrace.GetFrame(deepFrame)?.GetMethod();
-
-                        if (deepMethod?.DeclaringType != topMethod?.DeclaringType && deepMethod?.DeclaringType?.Namespace == topMethod?.DeclaringType?.Namespace)
-                        {
-                            return deepMethod;
-                        }
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                // Ignore exceptions in Release. The code above won't throw, but if it does, we don't want to crash the app.
-#if DEBUG
-                throw;
-#endif
-            }
-
-            return topMethod;
+            var methodName = stackTrace.GetFrame(3)?.GetMethod();
+            var className = methodName?.DeclaringType.Name;
+            return className + "::" + methodName?.Name;
         }
     }
 }
