325439efa7ca80c3f2e717f30ab79e99c820ac04
Shuai Yuan <128874481+shuaiyuanxx@users.noreply.github.com>
2025-02-21 09:58:54 +0800

    Add null check for colorPickerSettingsRepository

--- src/settings-ui/Settings.UI/ViewModels/ColorPickerViewModel.cs
+++ src/settings-ui/Settings.UI/ViewModels/ColorPickerViewModel.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation
+// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -56,6 +56,7 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 
             _settingsUtils = settingsUtils ?? throw new ArgumentNullException(nameof(settingsUtils));
 
+            ArgumentNullException.ThrowIfNull(colorPickerSettingsRepository);
             _colorPickerSettings = colorPickerSettingsRepository.SettingsConfig;
 
             InitializeEnabledValue();
